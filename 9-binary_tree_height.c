db10451c5292ac18736d6d069a391671d13bb9d8
teklemariam mossie <teklemariammossie1@gmail.com>
2023-03-06 03:06:43 -0800

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,13 +9,10 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t height_l = 0;
-	size_t height_r = 0;
-
-	if (!tree)
-		return (0);
-
-	height_l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	height_r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+return (0);
+if (tree->right && tree->left)
+return (2);
+else
+return (1);
 }
